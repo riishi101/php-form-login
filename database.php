af9e453a4d820fa647e8548593fe4d8005f256a8
Rishi Raijung <118293792+riishi101@users.noreply.github.com>
2024-09-10 00:22:41 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+
+   $db_server = "localhost";
+   $db_user = "root";
+   $db_pass = "";
+   $db_name = "businessdb";
+   $conn = "";
+
+   // handle error
+   try {
+    $conn = mysqli_connect($db_server, 
+                          $db_user, 
+                          $db_pass, 
+                          $db_name);
+   }
+   catch(mysqli_sql_exception){
+      echo "Could not connect! <br>";
+   }
+    if($conn){
+        echo "You are connected! <br>";
+    } 
+?>
